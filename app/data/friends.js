26b8db47c1b6173c5c715e7abf85d14d85571483
mevpadrino <manuelman.mev@gmail.com>
2019-07-09 23:16:20 -0500

    my first commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'roberto',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'3',
+			'5',
+			'2',
+			'1',
+			'5',
+			'4',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'pablo',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'3',
+			'4',
+			'5',
+			'1',
+			'1',
+			'3',
+			'1',
+			'2',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'jaime',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'3',
+			'1',
+			'2',
+			'5',
+			'4',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'ryley',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'5',
+			'3',
+			'2',
+			'4',
+			'3',
+			'4',
+			'1',
+			'5',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'crystal',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'4',
+			'1',
+			'2',
+			'4',
+			'5',
+			'5',
+			'3',
+			'5',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Julieth',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'3',
+			'1',
+			'3',
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'4',
+			'3'
+		]
+	},
+	{
+		name: 'adele',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'5',
+			'3',
+			'4',
+			'2',
+			'5',
+			'4',
+			'3',
+			'1',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'diorno',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'5',
+			'3',
+			'3',
+			'1',
+			'5',
+			'4',
+			'3',
+			'2',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Coco',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'4',
+			'5',
+			'3',
+			'5',
+			'5',
+			'3',
+			'5',
+			'4',
+			'5',
+			'2'
+		]
+	},
+	{
+		name: 'Tintin',
+		photo: 'https://www.pexels.com/photo/black-chimpanzee-smiling-50582/',
+		scores: [
+			'1',
+			'2',
+			'1',
+			'3',
+			'1',
+			'5',
+			'4',
+			'1',
+			'5',
+			'3'
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,23 @@
+//required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+ 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+//listening on PORT
+
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
